40cb0d084de8f6bcc5f7b678fc640e2d701e1993
dongyeop <hava12@naver.com>
2021-12-07 00:11:37 +0900

    item10 commit - 대칭성을 위반하는 코드

---
+++ src/item10/CaseInsensitiveString.java
@@ -0,0 +1,41 @@
+package item10;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+// 대칭성을 위배하는 코드
+public class CaseInsensitiveString {
+
+	public static void main(String[] args) {
+		CaseInsensitiveString cis =  new CaseInsensitiveString("Polish");
+		String s = "polish";
+
+		//대칭성 위배
+		System.out.println(cis.equals(s));
+		System.out.println(s.equals(cis));
+
+		List<CaseInsensitiveString> list = new ArrayList<>();
+		list.add(cis);
+
+		System.out.println(list.contains(s));
+	}
+
+	private final String s;
+
+	public CaseInsensitiveString(String s) {
+		this.s = Objects.requireNonNull(s);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof CaseInsensitiveString) {
+			return s.equalsIgnoreCase(((CaseInsensitiveString)obj).s);
+		}
+		if (obj instanceof String) { // 한 방향으로만 작동한다.
+			return s.equalsIgnoreCase((String) obj);
+		}
+		return false;
+	}
+}
+
